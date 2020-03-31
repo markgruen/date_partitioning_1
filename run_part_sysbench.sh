d4f774325579be62e124163a5bcbb0155c073748
markgruen <mgruen@mysqlsandbox1.server>
2020-03-31 18:49:34 -0400

    updated adding point select tests and added getopt for commandline options

--- run_part_sysbench.sh
+++ run_part_sysbench.sh
@@ -1,6 +1,54 @@
-threads=${1:-1}
-read_pct=${2:-60}
-engine=${3:-"innodb"}
+#!/bin/bash
+#
+# run benchmarks
+#
+
+function usage {
+   echo "Usage: $0 [--threads=THREADS] [--read_pct=READ_PCT] [--engine=ENGINE] [--point-select-pct=POINT_SELECT_PCT]
+   [--test-iterations=N] [--run-time=T] [--wait-time=W] [--outfile|-o FILE]" 1>&2
+   exit 1
+}
+
+SHORT=ho:
+LONG=threads:,read-pct:,engine:,point-select-pct:,test-iterations:,run-time:,wait-time:,outfile:
+
+OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
+
+if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
+
+eval set -- "$OPTS"
+
+# set initial values
+
+THREADS=1
+READ_PCT=60
+ENGINE=innodb
+POINT_SELECT_PCT=0
+TEST_ITERATIONS=3
+RUN_TIME=300
+WAIT_TIME=60
+OUTFILE="test_out_${ro}_${ENGINE}_${THREADS}"
+
+while true ; do
+    case "$1" in
+        --threads ) THREADS="$2"; shift 2;;
+        --read-pct ) READ_PCT="$2"; shift 2;;
+        --engine ) ENGINE="$2"; shift 2;;
+        --point-select-pct ) POINT_SELECT_PCT="$2"; shift 2;;
+        --test-iterations ) TEST_ITERATIONS="$2"; shift 2;;
+        --run-time ) RUN_TIME="$2"; shift 2;;
+        --wait-time ) WAIT_TIME="$2"; shift 2;;
+        --outfile|-o) OUTFILE="$2"; shift 2;;
+        -- ) shift; break ;;
+        * ) usage ;;
+    esac
+done
+
+echo "# STARTUP PARAMETERS threads: $THREADS read_pct: $READ_PCT point_select_pct: $POINT_SELECT_PCT engine: $ENGINE test_iterations: $TEST_ITERATIONS run-time: $RUN_TIME wait_time: $WAIT_TIME outfile: $OUTFILE"
+#exit 0
+
+# added to ease adding more app parameters
+
 
 if [ "$read_pct" -lt 0 -o "$read_pct" -gt 100 ]; then
     echo "ERROR: read_pct = $read_pct is an invalid value"
@@ -13,15 +61,8 @@ else
     ro="rw"
 fi
 
-#if [[ "$engine" != "" ]]; then
-#    engine="_${engine}"
-#fi
-
-out_file=${4:-"test_out_${ro}_${engine}_${threads}"}
-write_pct=$(( 100-$read_pct ))
-test_iterations=3
-run_time=300
-wait_time=60
+write_pct=$(( 100-$READ_PCT ))
+read_pct=$READ_PCT
 
 if [[ -z "$4" ]]; then
     TEE="tee"
@@ -29,102 +70,105 @@ else
     TEE="tee -a"
 fi
 
-#if [[ "$engine" == "tokudb" ]]; then
-#    compress="YES"
-#else
-#    compress="NO"
-#fi
 
 (
 echo "show create table big;" | ./use test
-for i in $(seq 1 $test_iterations)
+for i in $(seq 1 $TEST_ITERATIONS)
 do
     echo "########### PARTITION RW ${read_pct} ${write_pct}"
-    echo "THREAD: $threads"
+    echo "THREAD: $THREADS"
     echo "READ/WRITE: ${read_pct}:${write_pct}"
     echo "PARTITIONED: YES"
     echo "COMPRESSED: NO"
-    echo "ENGINE: $engine"
+    echo "POINT_SELECT_PCT: $POINT_SELECT_PCT"
+    echo "ENGINE: $ENGINE"
     date '+START: %Y-%m-%d %H:%M:%S'
 
-    sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$run_time --report-interval=5 \
+    sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$RUN_TIME --report-interval=5 \
     --end-date='2019-11-01 00:00:00' \
-    --threads=$threads \
+    --threads=$THREADS \
+    --point_select_id=$POINT_SELECT_PCT \
     --histogram=on \
     --read_pct=$read_pct run
 
     date '+END: %Y-%m-%d %H:%M:%S'
-    sleep $wait_time
+    sleep $WAIT_TIME
 done
 
 echo "PREPARING FOR NON PARTITION"
 echo "rename table big to _big_part, _big_nonpart to big;" | ./use -vvv test
 echo "show create table big;" | ./use test
-for i in $(seq 1 $test_iterations)
+for i in $(seq 1 $TEST_ITERATIONS)
 do
     echo "########### NON PARTITION RW ${read_pct} ${write_pct}"
-    echo "THREAD: $threads"
+    echo "THREAD: $THREADS"
     echo "READ/WRITE: ${read_pct}:${write_pct}"
     echo "PARTITIONED: NO"
     echo "COMPRESSED: NO"
-    echo "ENGINE: $engine"
+    echo "POINT_SELECT_PCT: $POINT_SELECT_PCT"
+    echo "ENGINE: $ENGINE"
     date '+START: %Y-%m-%d %H:%M:%S'
 
-    sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$run_time --report-interval=5 \
+    sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$RUN_TIME --report-interval=5 \
     --end-date='2019-11-01 00:00:00' \
-    --threads=$threads \
+    --threads=$THREADS \
+    --point_select_id=$POINT_SELECT_PCT \
     --histogram=on \
     --read_pct=$read_pct run
 
     date '+END: %Y-%m-%d %H:%M:%S'
-    sleep $wait_time
+    sleep $WAIT_TIME
 done
 
-if [[ "$engine" == "innodb" ]]; then
+if [[ "$ENGINE" == "innodb" ]]; then
     echo "PREPARING FOR PARTITION COMPRESSED"
     echo "rename table big to _big_nonpart, _big_comp to big;" | ./use -vvv test
     echo "show create table big;" | ./use test
-    for i in $(seq 1 $test_iterations)
+    for i in $(seq 1 $TEST_ITERATIONS)
     do
         echo "########### PARTITION COMPRESSED RW ${read_pct} ${write_pct}"
-        echo "THREAD: $threads"
+        echo "THREAD: $THREADS"
         echo "READ/WRITE: ${read_pct}:${write_pct}"
         echo "PARTITIONED: YES"
         echo "COMPRESSED: YES"
-        echo "ENGINE: $engine"
+        echo "POINT_SELECT_PCT: $POINT_SELECT_PCT"
+        echo "ENGINE: $ENGINE"
         date '+START: %Y-%m-%d %H:%M:%S'
 
-        sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$run_time --report-interval=5 \
+        sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$RUN_TIME --report-interval=5 \
         --end-date='2019-11-01 00:00:00' \
-        --threads=$threads \
+        --threads=$THREADS \
+        --point_select_id=$POINT_SELECT_PCT \
         --histogram=on \
         --read_pct=$read_pct run
 
         date '+END: %Y-%m-%d %H:%M:%S'
-        sleep $wait_time
+        sleep $WAIT_TIME
     done
 
     echo "PREPARING FOR NON PARTITION COMPRESSED"
     echo "rename table big to _big_comp, _big_comp_nonpart to big;" | ./use -vvv test
     echo "show create table big;" | ./use test
-    for i in $(seq 1 $test_iterations)
+    for i in $(seq 1 $TEST_ITERATIONS)
     do
         echo "########### NON PARTITION COMPRESSED RW ${read_pct} ${write_pct}"
-        echo "THREAD: $threads"
+        echo "THREAD: $THREADS"
         echo "READ/WRITE: ${read_pct}:${write_pct}"
         echo "PARTITIONED: NO"
         echo "COMPRESSED: YES"
-        echo "ENGINE: $engine"
+        echo "POINT_SELECT_PCT: $POINT_SELECT_PCT"
+        echo "ENGINE: $ENGINE"
         date '+START: %Y-%m-%d %H:%M:%S'
 
-        sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$run_time --report-interval=5 \
+        sysbench mg_part_workload.lua --mysql-ssl=off --mysql-user=msandbox --mysql-password=msandbox --mysql-port=5728 --mysql-host=127.0.0.1 --mysql-db=test --time=$RUN_TIME --report-interval=5 \
         --end-date='2019-11-01 00:00:00' \
-        --threads=$threads \
+        --threads=$THREADS \
+        --point_select_id=$POINT_SELECT_PCT \
         --histogram=on \
         --read_pct=$read_pct run
 
         date '+END: %Y-%m-%d %H:%M:%S'
-        sleep $wait_time
+        sleep $WAIT_TIME
     done
     echo "RESETTING BIG TO PARTITIONED"
     echo "rename table big to _big_comp_nonpart, _big_part to big;" | ./use -vvv test
--- run_tests.sh
+++ run_tests.sh
@@ -1,37 +1,78 @@
-now=$(date +"%Y%m%d_%H:%M:%S")
-first=${1:-"innodb"}
-#outfile=${2:-""}
-sleep_time=${2:-60}
+#!/bin/bash
+#
+# run benchmarks test suite
+#
 
-outfile="testout_$now"
+function usage {
+   echo "Usage: $0 [--first-engine=ENGINE] [--sleep-time=SLEEP] [--outfile|-o FILE]" 1>&2
+   exit 1
+}
+
+SHORT=ho:
+LONG=first-engine:,sleep-time:,outfile:
+
+OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
+
+if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
+
+eval set -- "$OPTS"
+
+# set initial values
+
+FIRST=innodb
+SLEEP=60
+FILE="testout_$(date +"%Y%m%d_%H:%M:%S")"
+
+while true ; do
+    case "$1" in
+        --first-engine ) FIRST="$2"; shift 2;;
+        --sleep-time ) SLEEP="$2"; shift 2;;
+        --outfile|-o ) FILE="$2"; shift 2;;
+        -- ) shift; break ;;
+        * ) usage ;;
+    esac
+done
+
+echo "
+FIRST: $FIRST
+SLEEP: $SLEEP
+FILE: $FILE"
 
 function innodb
 {
 echo "----------- INNODB --------------"
-./run_part_sysbench.sh 1 60 innodb "$outfile"
-sleep "$sleep_time"
-./run_part_sysbench.sh 1 100 innodb "$outfile"
-sleep "$sleep_time"
-./run_part_sysbench.sh 3 60 innodb "$outfile"
-sleep "$sleep_time"
-./run_part_sysbench.sh 3 100 innodb "$outfile"
-sleep "$sleep_time"
+./run_part_sysbench.sh --threads 1 --read-pct 60 --engine innodb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 1 --read-pct 100 --engine innodb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 1 --read-pct 100  --point-select-pct=100 --engine innodb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 3 --read-pct 60 --engine innodb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 3 --read-pct 100 --engine innodb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 3 --read-pct 100  --point-select-pct=100 --engine innodb "$FILE"
+sleep "$SLEEP"
 }
 
 function tokudb
 {
 echo "----------- TOKUDB --------------"
-./run_part_sysbench.sh 1 60 tokudb "$outfile"
-sleep "$sleep_time"
-./run_part_sysbench.sh 1 100 tokudb "$outfile"
-sleep "$sleep_time"
-./run_part_sysbench.sh 3 60 tokudb "$outfile"
-sleep "$sleep_time"
-./run_part_sysbench.sh 3 100 tokudb "$outfile"
-sleep "$sleep_time"
+./run_part_sysbench.sh --threads 1 --read-pct 60 --engine tokudb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 1 --read-pct 100 --engine tokudb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 1 --read-pct 100 --point-select-pct=100 --engine tokudb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 3 --read-pct 60 --engine tokudb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 3 --read-pct 100 --engine tokudb "$FILE"
+sleep "$SLEEP"
+./run_part_sysbench.sh --threads 3 --read-pct 100 --point-select-pct=100 --engine tokudb "$FILE"
+sleep "$SLEEP"
 }
 
-if [[ "$first" == "innodb" ]]; then
+if [[ "$FIRST" == "innodb" ]]; then
     innodb
     ./big_change_engine.sh tokudb
     tokudb
